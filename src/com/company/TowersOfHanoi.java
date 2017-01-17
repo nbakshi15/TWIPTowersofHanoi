d612401ff0dacb6a29af2675ee9cff52804b7c6c
nbakshi15 <nirali.bakshi@yahoo.com>
2017-01-17 10:31:56 -0500

    hello

---
+++ src/com/company/TowersOfHanoi.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by nb035 on 1/17/17.
+ */
+public class TowersOfHanoi {
+    constructor:
+    accepts total # of disks
+
+    solve method
+    move the tower
+    recursive method is going to show up
+//Move the specified number of disks from one tower to another by moving a subtower of n-1 disks out of the way, moving one disk, then moving the subtower back. Base case of 1 disk
+            moveOneDisk
+    SOP instructions on how to move.
+
+}
