5f7e9ae732259408d2ab3844e767724a6506a366
nbakshi15 <nirali.bakshi@yahoo.com>
2017-01-17 10:33:38 -0500

    dsdfsdfsdfs

--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -4,7 +4,7 @@ package com.company;
  * Created by nb035 on 1/17/17.
  */
 public class TowersOfHanoi {
-    constructor:
+    /*constructor:
     accepts total # of disks
 
     solve method
@@ -13,5 +13,5 @@ public class TowersOfHanoi {
 //Move the specified number of disks from one tower to another by moving a subtower of n-1 disks out of the way, moving one disk, then moving the subtower back. Base case of 1 disk
             moveOneDisk
     SOP instructions on how to move.
-
+*/
 }
