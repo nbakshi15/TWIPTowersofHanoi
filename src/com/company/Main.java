4f06a0a463f153c82799933206cba85a7fdef008
nbakshi15 <nirali.bakshi@yahoo.com>
2017-01-26 21:44:33 -0500

    Why do computers hate me so much

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,29 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Create object:
-        Invoke method solve
+        Scanner input = new Scanner(System.in);
+        int totDisks = 0;
+
+        System.out.println("Tower of Hanoi Game.");
+        System.out.println("This program will solve this puzzle for you.");
+        System.out.println("Please input number of disks to solve for:");
+        while(totDisks < 1)
+        {
+            try
+            {
+                totDisks = input.nextInt();
+                if(totDisks < 1)
+                    throw new Exception();
+            }
+            catch(Exception e)
+            {System.out.println("The amount of disks you want must be higher then 1");}
+        }
+        TowersOfHanoi tower = new TowersOfHanoi(totDisks);
+        System.out.println("Instructions:");
+        tower.solve();
+        System.out.println("Finished");
+    }
 
     }
-}
--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -14,4 +14,34 @@ public class TowersOfHanoi {
             moveOneDisk
     SOP instructions on how to move.
 */
+
+
+    public TowersOfHanoi(int x){
+        disks = x;
+        tower = new int[x];
+        for(int i = 0; i < tower.length; i++)
+        {
+            tower[i] = 1;
+        }
+        counter = 0;
+    }
+    public void solve(){
+        for(int i = 1; i <= disks; i++)
+            move(i);
+    }
+    public void move(int size) {
+        if (size > 0) {
+            counter++;
+            System.out.print("Turn " + counter + ": Move one disk from tower " + tower[size - 1] + " to tower ");
+            tower[size - 1] = (tower[size - 1] + (disks % 2 == size % 2 ? -1 : 1) + 3) % 3;
+            if (tower[size - 1] == 0)
+                tower[size - 1] = 3;
+            System.out.println(tower[size - 1]);
+            for (int i = 1; i < size; i++)
+                move(i);
+        }
+    }
+        public int disks;
+        public int[] tower;
+        public int counter;
 }
